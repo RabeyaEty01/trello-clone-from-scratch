5cbd8f97e8f388a1b6226f874e81e6f4299e11bf
rabeyaety01 <etyrabeya01@gmail.com>
2022-04-17 15:27:28 +0600

    add new column functionality added

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,15 +1,26 @@
 import { isEmpty } from "lodash";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Container, Draggable } from "react-smooth-dnd";
 import { initialData } from "../../actions/initialData";
 import { applyDrag } from "../../utilities/dragDrop";
 import { mapOrder } from "../../utilities/sorts";
 import Column from "../Column/Column";
+import cross from "/public/images/cross.svg";
 import plus from "/public/images/PLUS.svg";
+
 const BoardContent = () => {
   const [board, setBoard] = useState({});
   const [columns, setColumns] = useState([]);
+  const [openNewColumnsForm, setOpenNewColumnForm] = useState(false);
+
+  const newColumnInputRef = useRef(null);
+  const [newColumnTitle, setNewColumnTitle] = useState("");
+
+  const onNewColumnTitleChange = useCallback(
+    (e) => setNewColumnTitle(e.target.value),
+    []
+  );
 
   useEffect(() => {
     const boardFromDB = initialData.boards.find(
@@ -22,6 +33,12 @@ const BoardContent = () => {
       setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, "id"));
     }
   }, []);
+
+  useEffect(() => {
+    if (newColumnInputRef && newColumnInputRef.current) {
+      newColumnInputRef.current.focus();
+    }
+  }, [openNewColumnsForm]);
 
   if (isEmpty(board)) {
     return <div className="not-found">Board not found</div>;
@@ -50,6 +67,33 @@ const BoardContent = () => {
     }
   };
 
+  const toggleOpenNewColumnForm = () => {
+    setOpenNewColumnForm(!openNewColumnsForm);
+  };
+
+  const addNewColumn = () => {
+    if(!newColumnTitle){
+      newColumnInputRef.current.focus();
+      return
+    }
+    const newColumnToAdd={
+      id:Math.random().toString(36).substr(2,5), //5 random characters will remove when we implement code api
+      boardId:board.id,
+      title:newColumnTitle.trim(),
+      cardOrder:[],
+      cards:[]
+    }
+    let newColumns =[...columns]
+    newColumns.push(newColumnToAdd)
+
+    let newBoard={...board}
+    newBoard.columnOrder=newColumns.map(c=>c.id)
+    newBoard.columns=newColumns;
+    setColumns(newColumns);
+    setBoard(newBoard);
+    console.log(newColumnTitle)
+  };
+
   return (
     <div className="board-content">
       <Container
@@ -69,12 +113,44 @@ const BoardContent = () => {
           </Draggable>
         ))}
       </Container>
-      <div className="add-new-column">
-        <div className="d-flex">
-          <div className="icon">
-            <Image src={plus} alt="" width="14px" height="14px" />
-          </div>
-          <span>Add another column</span>
+
+      <div className="trello-container">
+        <div className="row">
+          {!openNewColumnsForm && (
+            <div className="col">
+              <div className="add-new-column" onClick={toggleOpenNewColumnForm}>
+                <div className="d-flex">
+                  <div className="icon">
+                    <Image src={plus} alt="" width="14px" height="14px" />
+                  </div>
+                  <span>Add another column</span>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {openNewColumnsForm && (
+            <div className="row">
+              <div className="col ">
+                <div className="enter-new-column">
+                  <input
+                    className="form-control form-control-sm input-enter-new-column"
+                    type="text"
+                    placeholder="Enter column title..."
+                    ref={newColumnInputRef}
+                    value={newColumnTitle}
+                    onChange={onNewColumnTitleChange}
+                  />
+                  <button className="btn btn-success" onClick={addNewColumn}>
+                    Add column
+                  </button>
+                  <span className="cancel-new-column">
+                    <Image src={cross} alt="" width="14px" height="14px" />
+                  </span>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
